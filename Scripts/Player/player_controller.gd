171ceb83c58e2b1957232519ba768f820e58a9ce
DrakBak1 <JMToves1118@outlook.com>
2025-08-12 17:21:46 -0700

    Jarryd change

--- Scripts/Player/player_controller.gd
+++ Scripts/Player/player_controller.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 800.0
+const Acceleration = 1000 #Jarryd Added
+const friction = 3500 #Jarryd Added
+const JUMP_VELOCITY = -500.0
 
 @onready var handL: Marker2D = $PlayerIKBody/CharacterContainer/Body/GBotForearmL/HandL
 @onready var handR: Marker2D = $PlayerIKBody/CharacterContainer/Body/GBotForearmR/HandR
@@ -10,7 +12,7 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * 2 * delta #changed gravity
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump1") and is_on_floor():
@@ -20,8 +22,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left1", "Right1")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, Acceleration * delta)#Jarryd Change
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, friction * delta)#Jarryd Change
 	
 	move_and_slide()
