2149109a85f88b5bab0b89e995761318b9fa9de4
Dylan <DylanRayPulley@gmail.com>
2025-09-19 20:06:10 -0700

    Headache

---
+++ Scripts/chad_room_start.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		var generator: LevelGenerator = get_tree().get_first_node_in_group("ChadGenerator")
+		generator.CurrentRoom += 1
+		queue_free()
